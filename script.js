7342586468680250606d1296c43a882a09afff21
Tiger :D <tigexd@protonmail.com>
2026-03-13 19:52:07 +0000

    Update file script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function changePageTitle() {
         };
     document.querySelector('title').textContent
         = newPageTitle;
-
+    }
 
         var input = document.getElementById('command-input');
 input.addEventListener("keypress", function(event) {
@@ -19,7 +19,6 @@ input.addEventListener("keypress", function(event) {
     document.getElementById("trigBtn").click();
   }
 });
-}
 
 
 
