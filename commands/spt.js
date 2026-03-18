3ba70efe112c696bc0023eb4ca0a446cb34adb22
Tiger :D <tigexd@protonmail.com>
2026-03-18 20:25:23 +0000

    Edit spt.js

--- commands/spt.js
+++ commands/spt.js
@@ -2,27 +2,48 @@ let packagesInstalled = 2;
 let packages = ["bwash", "sudo"];
 const paksAvail = ["hyfetch", "htop"];
 
+const pakInfo = {
+    "sudo": { type: "pkg", version: "1.1.13.p2" },
+    "bwash": { type: "shell", version: "1.13.0" },
+    "hyfetch": { type: "pkg", version: "2.0.1" },
+    "htop": { type: "pkg", version: "2.3.1" },
+};
+
+
+
+
 function addPak(pakName) {
-    if (paksAvail.includes(target)) {
+    if (paksAvail.includes(pakName)) {
         packagesInstalled += 1;
-        packages.push(target);
+        packages.push(pakName);
     } else {
         printLine(`bwash: spt: Unable to locate package '${target}' in https://gitlab.com/TigeXD/package-clump`);
     }
 }
 
 function delPak(pakName) {
-    if (packages.includes(target)) {
+    if (packages.includes(pakName)) {
         packagesInstalled -= 1;
-        packages.pop(target);
+        packages.pop(pakName);
     } else {
-        printLine(`bwash: spt: Unable to locate package '${target}' in `);
+        printLine(`bwash: spt: Unable to locate package '${target}' in /user/bin`);
     }
-    
+
     packagesInstalled -= 1;
     const packages = list.filter(item => item !== pakName);
 }
 
+function sptUpdate() {
+    localStorage.setItem('packagesInstalled', packagesInstalled);
+}
+
+function sptUpgrade(pakName) {
+    if (paksAvail.includes(pakName)) {
+        printLine(`bwash: spt: woah woah, too fast buddy, this feature is not ready yet`);
+    } else {
+        printLine(`bwash: spt: Unable to locate package '${target}' in /user/bin`);
+    }
+}
 
 commands['spt'] = {
     level: 1,
@@ -30,15 +51,14 @@ commands['spt'] = {
         const action = args.join(' ');
         if (action === 'install') {
             const target = args.join(' ');
-            addPak()
-        } else if (output === 'update') {
-            function sptUpdate() {
-                localStorage.setItem('packagesInstalled', packagesInstalled);
-            }
-        } else if (output === 'upgrade') {
-
-        } else if (output === 'remove') {
-
+            addPak(target);
+        } else if (action === 'update') { // just a localStorage refresher
+            sptUpdate();
+        } else if (action === 'upgrade') { // also just a fancy thingy
+        const target = args.join(' ');
+            sptUpgrade(target);
+        } else if (action === 'remove') {
+            delPak(target);
         }
     }
 };
