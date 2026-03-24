19cf83a875a60763694f2c4cff628a46bc8a5e14
Tiger :D <tigexd@protonmail.com>
2026-03-24 19:53:21 +0000

    Update file script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ input.addEventListener("keypress", function (event) {
 function dynamicallyLoadScript(url) {
     var script = document.createElement("script");  // create a script DOM node
     script.src = url;  // set its src to the provided URL
-
+    
     document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
 // end :3
@@ -299,6 +299,7 @@ const commands = {
         level: 1,
         execute: (args) => {
             dynamicallyLoadScript('commands/spt.js');
+            dynamicallyLoadScript('packages/hyfetch.js'); // for the sake of demo for now
         }
     }
 
